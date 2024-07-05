f030849b8862c6c81bcc681cfe84da02adbb0624
Rakha Ahmad S <120225395+itsabes@users.noreply.github.com>
2024-07-05 09:52:23 +0700

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,8 @@
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
 $config['smtp_user'] = 'synergy.sawahbesar@gmail.com';
-// $config['smtp_pass'] = 'b15m1ll4h';
-$config['smtp_pass'] = 'pielmbdeodohdpsm';
+$config['smtp_pass'] = 'b15m1ll4h';
+// $config['smtp_pass'] = 'pielmbdeodohdpsm';
 $config['smtp_port'] = 465;
  
 $config['charset'] = 'utf-8';
